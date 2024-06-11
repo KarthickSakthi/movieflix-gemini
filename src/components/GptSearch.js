f7034656654c7597d235686a64ee8ff8e8a07b04
Karthick S <karthicksakthi124@gmail.com>
2024-06-11 03:26:04 +0000

    Gpt Search UI and Multilingual support added

---
+++ src/components/GptSearch.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { GptSearchBar } from "./GptSearchBar";
+import { GptMovieSuggestion } from "./GptMovieSuggestion";
+import { BACKGROUND_IMAGE_SIGNIN } from "../constants";
+
+export function GptSearch(){
+     return (
+       <>
+         <div className="fixed -z-10">
+           <img
+             className="h-screen object-cover"
+             src={BACKGROUND_IMAGE_SIGNIN}
+             alt="logo"
+           />
+         </div>
+         <div className="">
+           <GptSearchBar />
+           <GptMovieSuggestion />
+         </div>
+       </>
+     );
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,13 @@ import { auth } from "../utils/firebase.js";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../store/userSlice.js";
-import { LOGO } from "../constants.js";
+import { LOGO, SUPPORTED_LANGUAGES } from "../constants.js";
+import { toggleGptSearchView } from "../store/gptSlice.js";
+import { changeLanguage } from "../store/configSlice.js";
 export function Header() {
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store)=>store.gpt.showGptSearch)
   const dispatch = useDispatch();
   useEffect(function listenAuthChange() {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -35,19 +38,47 @@ export function Header() {
         navigate("/error");
       });
   }
+
+  function handleGptSearch() {
+    dispatch(toggleGptSearchView());
+  }
+
+
+
+  function handleLanguageChange(event) {
+    dispatch(changeLanguage(event.target.value))
+  }
   return (
     <div className=" absolute z-10 w-screen px-8 py-2 bg-gradient-to-b from-black flex justify-between">
       <img className=" w-44" src={LOGO} alt="logo" />
 
       {user && (
         <div className="flex gap-2 items-center">
-          <img src={user?.photoURL} alt="sign out" className="  w-10 h-10" />
+
+         { showGptSearch && 
+          <select
+            className="p-2 m-2 bg-gray-900 text-white"
+            onChange={handleLanguageChange}
+          >
+            {SUPPORTED_LANGUAGES.map((lang) => (
+              <option key={lang.identifier} value={lang.identifier}>
+                {lang.name}
+              </option>
+            ))}
+          </select> }
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearch}
+          >
+          {showGptSearch ? "Home Page" :"GPT Search"}  
+          </button>
           <button
             className=" bg-red-700 font-bold text-white p-2  rounded-lg"
             onClick={handleSignOut}
           >
             (Sign out)
           </button>
+          <img src={user?.photoURL} alt="sign out" className="  w-10 h-10" />
         </div>
       )}
     </div>
